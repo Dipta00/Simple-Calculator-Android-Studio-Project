e881e89e4aaf0fd80bd0bb7915408c7752d14198
Dipta00 <dip222896@gmail.com>
2021-11-29 19:21:37 +0600

    here is my file

---
+++ calculator.java
@@ -0,0 +1,19 @@
+class Bicycle {
+
+    // state or field
+    private int gear = 5;
+  
+    // behavior or method
+    public void Bicycle() {
+      System.out.println("bicycle");
+    }
+
+    public void getter() {
+        System.out.println("getter function");
+      }
+
+      public void setter() {
+        System.out.println("here is setter function");
+      }
+
+  }
